bbe75bf0e8701451a99ca9cd4676542dbb88185a
Fronees <anbo.you77@gmail.com>
2025-05-05 17:00:07 +0300

    cpp.cpp

--- homeworke/cpp.cpp
+++ homeworke/cpp.cpp
@@ -1,29 +1,30 @@
 #include <iostream>
+using namespace std;
 
 void vstavka(int*& arr1, int& size1, int* arr2, int size2) {
     int newSize = size1 + size2;
     int* tempArr = new int[newSize];
-    for (int i = 0; i < size1; ++i) 
+    for (int i = 0; i < size1; ++i)
     {
         tempArr[i] = arr1[i];
     }
-    for (int i = 0; i < size2; ++i) 
+    for (int i = 0; i < size2; ++i)
     {
         tempArr[size1 + i] = arr2[i];
     }
     int newnewSize = 0;
-    for (int i = 0; i < newSize; ++i) 
+    for (int i = 0; i < newSize; ++i)
     {
         bool isDuplicate = false;
-        for (int j = 0; j < newnewSize; ++j) 
+        for (int j = 0; j < newnewSize; ++j)
         {
-            if (tempArr[i] == tempArr[j]) 
+            if (tempArr[i] == tempArr[j])
             {
                 isDuplicate = true;
                 break;
             }
         }
-        if (!isDuplicate) 
+        if (!isDuplicate)
         {
             tempArr[newnewSize++] = tempArr[i];
         }
@@ -32,7 +33,7 @@ void vstavka(int*& arr1, int& size1, int* arr2, int size2) {
     delete[] arr1;
     arr1 = new int[newnewSize];
 
-    for (int i = 0; i < newnewSize; ++i) 
+    for (int i = 0; i < newnewSize; ++i)
     {
         arr1[i] = tempArr[i];
     }
@@ -41,21 +42,34 @@ void vstavka(int*& arr1, int& size1, int* arr2, int size2) {
     delete[] tempArr;
 }
 
-int main() 
+int main()
 {
-    int size1 = 6;
-    int* arr1 = new int[size1] {1, 3, 5, 3, 9, 15};
-    int size2 = 5;
-    int* arr2 = new int[size1] { 78, 6, 7, 4, 5 }; // сюда надо писать значение для добавления
-
+    cout << "write size of arr1: " << endl;
+    int size1 = 0;
+    cin >> size1;
+    int* arr1 = new int[size1];
+    cout << "Enter elements arr1: ";
+    for (int i = 0; i < size1; i++)
+    {
+        cin >> arr1[i];
+    }
+    cout << "write size of arr2: " << endl;
+    int size2 = 0;
+    cin >> size2;
+    int* arr2 = new int[size2];
+    cout << "Enter elements arr2: ";
+    for (int i = 0; i < size2; i++)
+    {
+        cin >> arr2[i];
+    }
     vstavka(arr1, size1, arr2, size2);
 
     std::cout << "rezult: ";
-    for (int i = 0; i < size1; ++i) 
+    for (int i = 0; i < size1; ++i)
     {
         std::cout << arr1[i] << " ";
     }
 
-    delete[] arr1; 
+    delete[] arr1;
     return 0;
 }
